65b041e2bb7c6a7c5f2ac8eecde6c341e869e979
jingyang <zhoujingyang@gomeholdings.com>
2017-12-24 23:15:57 +0800

    add Chapter9

---
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -0,0 +1,139 @@
+package com.course.httpclient.cookies;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class MyCookies {
+
+    @Test
+    public void getCookies() throws IOException {
+        String url = "http://localhost:9999/post/with/cookies";
+
+        DefaultHttpClient client = new DefaultHttpClient();
+        JSONObject paramJson = new JSONObject();
+        paramJson.put("zhangsan","20");
+        StringEntity entity = new StringEntity(paramJson.toString(),"utf-8");
+        entity.setContentType("application/json");
+        HttpPost post = new HttpPost(url);
+
+
+        post.setEntity(entity);
+        HttpResponse response = client.execute(post);
+        System.out.println(response.getEntity());
+
+        CookieStore store = client.getCookieStore();
+
+        List<Cookie> cookieList = store.getCookies();
+
+        for(Cookie cookie : cookieList){
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookie name =" + name + "cookie value =" + value);
+        }
+
+    }
+
+    /**
+     * 获取无参数get方法中的cookies
+     * @throws IOException
+     */
+    @Test
+    public void getCookiesForGetMethod() throws IOException {
+        String url = "http://localhost:9999/getCookies";
+
+        CookieStore store = getCookieStore(url);
+
+        List<Cookie> cookieList = store.getCookies();
+
+        for(Cookie cookie : cookieList){
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookie name =" + name + "     cookie value =" + value);
+        }
+    }
+
+    /**
+     * 获取cookiestore
+     * @param url
+     * @return
+     * @throws IOException
+     */
+    private CookieStore getCookieStore(String url) throws IOException {
+
+        DefaultHttpClient client = new DefaultHttpClient();
+
+        HttpGet get = new HttpGet(url);
+
+        client.execute(get);
+
+        return client.getCookieStore();
+    }
+
+
+    /**
+     * 携带cookies访问
+     * @throws IOException
+     */
+    @Test
+    public void getCookiesForGetMethodWithHeader() throws IOException {
+        //获取cookies的url
+        String cookiesUrl = "http://localhost:9999/getCookies";
+
+        //需要携带cookies的请求
+        String url = "http://localhost:9999/get/with/cookies";
+
+        DefaultHttpClient client = new DefaultHttpClient();
+
+        HttpGet get = new HttpGet(url);
+
+        client.setCookieStore(getCookieStore(cookiesUrl));//获取依赖的cookies
+
+        HttpResponse response = client.execute(get);
+
+        //获取响应码
+        int statuscode = response.getStatusLine().getStatusCode();
+
+        System.out.println(statuscode);
+
+        //判断响应状态
+        if(statuscode==200){
+            //获取响应内容
+            String result = getContent(response);
+            System.out.println(result);
+        }
+    }
+
+
+    /**
+     * 获取响应内容
+     * @param response
+     * @return
+     * @throws IOException
+     */
+    public String getContent(HttpResponse response) throws IOException {
+        String result = null;
+
+        InputStream in = response.getEntity().getContent();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+        StringBuffer stringBuffer = new StringBuffer("");
+        String line = "";
+        while((line = bufferedReader.readLine())!=null){
+            stringBuffer.append(line);
+        }
+        result= stringBuffer.toString();
+        return result;
+    }
+}
